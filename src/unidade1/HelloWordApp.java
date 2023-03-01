542225987a243d159c065f3c0c2ca23d2342e9b4
Aledrizzato78 <Latitude 5280@DESKTOP-GI98INL>
2023-03-01 08:42:52 -0300

    Hello Word

---
+++ src/unidade1/HelloWordApp.java
@@ -0,0 +1,14 @@
+package unidade1;
+
+import javax.swing.JOptionPane;
+
+public class HelloWordApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		JOptionPane.showMessageDialog(null, "Hello World!");
+		
+	}
+
+}
