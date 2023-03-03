9389186bd3559e3dd1d04fa3f9402a3d95db19d9
Aledrizzato78 <aledrizzato@gmail.com>
2023-03-03 11:00:30 -0300

    projetinho

--- src/Unidade4/Desenhista.java
+++ src/Unidade4/Desenhista.java
@@ -2,6 +2,17 @@ package Unidade4;
 
 public class Desenhista {
 	
+import java.util.Date;
+	public class Circulo{
+		public string Circulo;
+		public class Quadrado{
+			public string Quadrado;
+			public class Triangulo{
+				public string Triangulo;
+			}
+		}
+		
+	
 	public static void main(String[] args) {
 	
 	}
