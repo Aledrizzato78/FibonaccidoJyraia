cad1218a8267084678c9f568a0577b5d41ad8f41
Aledrizzato78 <aledrizzato@gmail.com>
2023-03-03 11:29:50 -0300

    projetinho

--- src/Unidade4/Desenhista.java
+++ src/Unidade4/Desenhista.java
@@ -3,9 +3,7 @@ package Unidade4;
 public class Desenhista {
 		public static void main(String[] args) {
 			
-			Figura Circulo = new Circulo();
-			Figura Quadrado = new Quadrado();
-			Figura Triangulo = new Triangulo();
+			Figura figura = new desenho();
 	
 	}
 	
--- src/Unidade4/Figura.java
+++ src/Unidade4/Figura.java
@@ -2,5 +2,11 @@ package Unidade4;
 
 public class Figura extends Desenhista {
 	
+	public static void main(String[] args) {
 	
+		figura Circulo = new desenho();
+		figura Quadrado = new desenho1();
+		figura Triangulo = new desenho2();
+	
+	}
 }
