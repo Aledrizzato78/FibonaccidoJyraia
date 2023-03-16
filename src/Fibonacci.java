4c3e18c63a34799b83420c24da3722e3aee2d840
Aledrizzato78 <aledrizzato@gmail.com>
2023-03-16 12:44:53 -0300

    projeto Fibonacci

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -3,30 +3,28 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		
+		for (int cont; cont=(novoScanner.main(valor1))+(novoScanner.main(valor2)); cont++){
+			System.out.println(cont);
 		
 		
-		int anterior1 = 0;		
-		int anterior2 = 1;
-		int calculado = anterior1 + anterior2;
-		System.out.print(anterior1+ "," +anterior2+ "," +calculado+ ",");
+			int anterior1 = 0;		
+			int anterior2 = 1;
+			int calculado = anterior1 + anterior2;
+			
+		System.out.println(anterior1+ "," +anterior2+ "," +calculado+"," + (cont));
 		
-		for (int i=1; i <=5; i++)
+		for (int i; i=(novoScanner.main(valor1))+(novoScanner.main(valor2)); i++)
 		{
 			anterior1 = anterior2;
 			anterior2 = calculado;
 			calculado = anterior1 + anterior2;
-			System.out.print(calculado + ",");
-		
+			System.out.println(calculado+ ","+ (cont));
+				}
 		}
-		{
-			
-				for (int cont=1; cont <=8 ; cont++){
-				System.out.println("o valor do contador é:"+cont);
-			}
-			}
+		}
 	
-		}
-	}
+	
+}
 
 				
 
---
+++ src/novoScanner.java
@@ -0,0 +1,33 @@
+	
+	import java.util.Scanner;
+
+	public class novoScanner {
+	
+			public static int valor1;
+			public static int valor2;
+
+			public static void main(String[] args) {
+		
+		
+		
+		Scanner sc1 = new Scanner (System.in);
+		System.out.print("Informe o valor inicial de Fibonacci");
+		int valor1 = sc1.nextInt();
+		System.out.println(valor1);
+		
+		
+		Scanner sc2 = new Scanner (System.in);
+		System.out.print("Informe o valor final de Fibonacci");	
+		int valor2 = sc2.nextInt();{
+		System.out.println(valor2);
+		
+			
+
+					
+				
+			
+			}
+		}
+	
+	}
+	
