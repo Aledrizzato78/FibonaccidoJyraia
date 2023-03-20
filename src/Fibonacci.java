2bb5194eabcc233d0119563976ac282f83c8ba7c
Aledrizzato78 <aledrizzato@gmail.com>
2023-03-20 10:48:58 -0300

    nova alteração

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,30 +1,42 @@
 
-public class Fibonacci {
 
+
+	import java.util.Scanner;
+
+	public class Fibonacci {
+	
 	public static void main(String[] args) {
+	
+		Scanner sc1 = new Scanner (System.in);
+		System.out.print("Informe o valor inicial de Fibonacci");
+		double valor1 = sc1.nextDouble();
+		sc1.close();
 		
-		for (int cont; cont=(novoScanner.main(valor1))+(novoScanner.main(valor2)); cont++){
-			System.out.println(cont);
+		Scanner sc2 = new Scanner (System.in);
+		System.out.print("Informe o valor final de Fibonacci");	
+		double valor2 = sc2.nextDouble();{
+		sc2.close();
 		
 		
 			int anterior1 = 0;		
 			int anterior2 = 1;
 			int calculado = anterior1 + anterior2;
 			
-		System.out.println(anterior1+ "," +anterior2+ "," +calculado+"," + (cont));
+		System.out.println(anterior1+ "," +anterior2+ "," +calculado+",");
 		
-		for (int i; i=(novoScanner.main(valor1))+(novoScanner.main(valor2)); i++)
+		for ( double i = 0; i =(valor1 % valor2););
+
 		{
 			anterior1 = anterior2;
 			anterior2 = calculado;
 			calculado = anterior1 + anterior2;
-			System.out.println(calculado+ ","+ (cont));
-				}
+			System.out.println(calculado+ ",");
+				
 		}
 		}
-	
-	
-}
+	}
+	}
+
 
 				
 
